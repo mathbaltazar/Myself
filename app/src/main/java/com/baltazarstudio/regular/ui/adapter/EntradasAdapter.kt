ad8decaabc5e58401da78972c321169a206a54ca
matheus <matheusbaltazar@live.com>
2020-07-17 16:19:52 -0300

    Correção Lógica Expand Entradas

--- app/src/main/java/com/baltazarstudio/regular/ui/adapter/EntradasAdapter.kt
+++ app/src/main/java/com/baltazarstudio/regular/ui/adapter/EntradasAdapter.kt
@@ -17,73 +17,82 @@ import org.jetbrains.anko.toast
 
 class EntradasAdapter(context: Context, private val dao: EntradaDAO, dono: String) :
     RecyclerView.Adapter<RecyclerView.ViewHolder>() {
-
-
+    
+    
     private val layoutInflater: LayoutInflater
     private val entradas: ArrayList<Entrada>
     private var expanded = hashSetOf<Int>()
-
+    
     init {
         this.layoutInflater = LayoutInflater.from(context)
-        if (dono.equals(EntradasFragment.TODOS))
-            this.entradas = dao.getTodasEntradas()
-        else
-            this.entradas = dao.getTodasEntradas(dono)
+        if (dono.equals(EntradasFragment.TODOS)) this.entradas = dao.getTodasEntradas()
+        else this.entradas = dao.getTodasEntradas(dono)
     }
-
+    
     override fun getItemCount(): Int {
         return entradas.size
     }
-
+    
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         return ItemViewHolder(layoutInflater.inflate(R.layout.list_item_entradas, parent, false))
     }
-
+    
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         (holder as ItemViewHolder).bindView(position)
     }
-
+    
     private inner class ItemViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-
+        
         fun bindView(position: Int) {
             val entrada = entradas[position]
-
+            
             itemView.setOnClickListener {
-                if (expanded.contains(position))
-                    expanded.remove(position)
-                else
-                    expanded.add(position)
-                notifyItemChanged(position)
+                if (expanded.contains(adapterPosition)) expanded.remove(adapterPosition)
+                else expanded.add(adapterPosition)
+                notifyItemChanged(adapterPosition)
             }
-
+            
             itemView.setOnLongClickListener {
-                AlertDialog.Builder(itemView.context)
-                    .setTitle("Atenção")
+                AlertDialog.Builder(itemView.context).setTitle("Atenção")
                     .setMessage("Confirma a exclusão da entrada?")
                     .setPositiveButton("Sim") { _, _ ->
                         dao.remover(entrada)
                         itemView.context.toast("Excluído!")
                         entradas.remove(entrada)
                         notifyItemRemoved(adapterPosition)
-                    }
-                    .setNegativeButton("Não", null)
-                    .show()
+                        reassignExpanded(adapterPosition)
+                    }.setNegativeButton("Não", null).show()
                 true
             }
-
-            if (expanded.contains(position)) {
+            
+            if (expanded.contains(adapterPosition)) {
                 itemView.ll_item_entradas_info.visibility = View.VISIBLE
                 itemView.iv_item_entradas_expand_arrow.setImageResource(R.drawable.ic_arrow_up)
             } else {
                 itemView.ll_item_entradas_info.visibility = View.GONE
                 itemView.iv_item_entradas_expand_arrow.setImageResource(R.drawable.ic_arrow_down)
             }
-
+            
             itemView.tv_item_entradas_valor.text = Utils.formatCurrency(entrada.valor)
             itemView.tv_item_entradas_dono.text = entrada.dono
             itemView.tv_item_entradas_descricao.text = entrada.descricao
             itemView.tv_item_entradas_data.text = entrada.data?.formattedDate()
-
+            
+        }
+        
+        private fun reassignExpanded(position: Int) {
+            
+            val newExpaded = hashSetOf<Int>()
+            val iterator = expanded.iterator()
+            while (iterator.hasNext()) {
+                val expand = iterator.next()
+                if (expand > position) {
+                    newExpaded.add(expand - 1)
+                    iterator.remove()
+                }
+            }
+            if (newExpaded.isNotEmpty())
+                expanded.addAll(newExpaded)
         }
     }
 }
